01ca1b3fd6e8a7105f8ff1be0f1fa0ca374ba1f0
prathamesh palatshaha <prathamesh.palatshaha@vit.edu.in>
2024-07-05 23:45:40 +0530

    try-catch

---
+++ ExceptionHandling.java
@@ -0,0 +1,15 @@
+public class ExceptionHandling {
+public static void main(String[] args) {
+    //TRY - CATCH in Exception Handling
+
+    int [] marks = {90};
+
+    try {
+        System.out.println(marks[1]);
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+
+    System.out.println("THE END");
+}
+}
