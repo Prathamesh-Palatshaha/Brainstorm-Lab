7cdff00ab74002844f1b6a9865bb66d518a7139f
prathamesh palatshaha <prathamesh.palatshaha@vit.edu.in>
2024-07-05 22:56:54 +0530

    updated

--- Basic.java
+++ Basic.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Basic {
 public static void main(String[] args) {
     /*
@@ -9,7 +11,7 @@ public static void main(String[] args) {
     <=
 
     */
-
+    System.out.println(" Comparison Operators: ");
     boolean isDay = false;
     if (isDay) System.out.println("Day");
     else System.out.println("Night");
@@ -18,5 +20,47 @@ public static void main(String[] args) {
 
     if (age > 18) System.out.println("Eligible to vote!");
     else System.out.println("Sorry.");
+
+    //Logical Operators
+    System.out.println("Logical Operators: ");
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Enter cash: ");
+    int cash = sc.nextInt();
+
+    // pen = 10 and nb = 30
+
+    if (cash < 10)
+    {
+        System.out.println("Cannot buy anything.");
+        System.out.println("Bring more cash");
+    }
+
+    else if (cash <40  && cash > 10){
+        System.out.println("Can buy 1 thing ");
+    }
+
+    else
+    {
+        System.out.println("Can buy any one or both");
+    }
+
+
+    // swtich statement
+    System.out.println("SWITCH: ");
+    int day = 1;
+    // 1 - monday 2 - tues
+
+    switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+        System.out.println("tuesday");
+        break;
+        default:
+            break;
+    }
+
 }
 }
