9a65ed5800815ccc3f1e92fb7706b9f913d1bcce
prathamesh palatshaha <prathamesh.palatshaha@vit.edu.in>
2024-07-05 20:24:36 +0530

    Comparison Operators and Conditional Statements

---
+++ Basic.java
@@ -0,0 +1,22 @@
+public class Basic {
+public static void main(String[] args) {
+    /*
+    Comparison Operators
+    ==
+    >
+    <
+    >=
+    <=
+
+    */
+
+    boolean isDay = false;
+    if (isDay) System.out.println("Day");
+    else System.out.println("Night");
+
+    int age = 30;
+
+    if (age > 18) System.out.println("Eligible to vote!");
+    else System.out.println("Sorry.");
+}
+}
